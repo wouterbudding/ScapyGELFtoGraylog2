83175d4b22708f27634b1ddf83766aad380126e3
wouterbudding <github@isabest.com>
2016-11-19 19:01:56 +0100

    Update sniff.py

--- sniff.py
+++ sniff.py
@@ -4,19 +4,26 @@
 from scapy.all import *
 from subprocess import call
 import time
-#IP and hostname where to send the GELF UDP packages to
-SentToHost = "192.0.2.1"
-SentToPort = "12201"
-#enter the location that you are running this sniffer from; like home, or Garlicstreet 3 or whatever
-location = "home"
 
+import logging
+import graypy
+
+#IP and port where to send the GELF UDP packages to
+SentToHost      = "192.0.2.1"
+SentToPort      = "12201"
+#enter the name of this sniffer
+Name            = "SniffPy"
+#enter the location that you are running this sniffer from; like home, or Garlicstreet 3 or whatever
+location        = "home"
+#what is the name of the SYSLOG level you want to use
+SyslogLevel     = 1
 
 print "---- INSTRUCTIONS ----"
 print " "
-print "This Python script will sniff three management frame subtypes in 802.11: 0, 2 and 4 and will send it to Graylog2. Make sure you first set the interface you want to use in monitoring mode, before starting this script: airmon-ng start wlan0. Also, make sure you have a GELF UDP listener ready on " + SentToHost + ":" + SentToPort + ". In the same directory as this script, store the file you get from https://gist.github.com/derlinkshaender/5995776 and name it graylogger.py."
+print "This script will sniff three management frame subtypes in 802.11: 0, 2 and 4 and will send it to Graylog2. Make sure you first set the interface you want to use in monitoring mode, before starting this script: airmon-ng start wlan0. Also, make sure you have a GELF UDP listener ready on " + SentToHost + ":" + SentToPort + ". In the same directory as this script, store the file you get from https://gist.github.com/derlinkshaender/5995776 and name it graylogger.py."
 print " "
 print "---- THIS PROGRAM IS \"AS IS\" WITHOUT WARRANTY OF ANY KIND ----"
-    
+
 # Define the interface name that we will be sniffing from, you can
 # change this if needed.
 interface = "mon0"
@@ -24,6 +31,11 @@ interface = "mon0"
 # Next, declare a Python list to keep track of client MAC addresses
 # that we have already seen so we only print the address once per client.
 observedclients = []
+
+my_logger = logging.getLogger(Name)
+my_logger.setLevel(logging.DEBUG)
+handler = graypy.GELFHandler(SentToHost, int(SentToPort))
+my_logger.addHandler(handler)
 
 # The sniffmgmt() function is called each time Scapy receives a packet
 # (we'll tell Scapy to use this function below with the sniff() function).
@@ -33,8 +45,8 @@ def sniffmgmt(p):
     # Define our tuple (an immutable list) of the 3 management frame
     # subtypes sent exclusively by clients. I got this list from Wireshark.
     stamgmtstypes = (0, 2, 4)
-    
-    
+
+
     # Make sure the packet has the Scapy Dot11 layer present
     if p.haslayer(Dot11):
 
@@ -48,9 +60,17 @@ def sniffmgmt(p):
             # client address isn't present, print the address and then add
             # it to our list.
             if p.addr2 not in observedclients:
-                localtime = time.asctime( time.localtime(time.time()) )
+                epoch           = int(time.time())
+                localtime       = time.asctime( time.localtime(time.time()) )
+                #print to stdout the time and the MAC
                 print localtime + " " + p.addr2
-                subprocess.call("./graylogger.py -l INFO -f \"SNIFFER\" -p " + SentToPort + " " + SentToHost + " " + p.addr2 + " -d \"epoch:`date +%s`\" -d \"client_mac:true\" -d location:" + location, shell=True)
+                #now send the GELF message
+                my_adapter = logging.LoggerAdapter(logging.getLogger(Name),
+                                                    { 'client_mac': 'true', 'location': Name, 'epoch': epoch })
+
+                my_logger.debug(p.addr2)
+
+                #subprocess.call("./graylogger.py -l INFO -f \"SNIFFER\" -p " + SentToPort + " " + SentToHost + " " + p.addr2 + " -d \"epoch:`date +%s`\" -d \"client_mac:true\" -d location:" + location, shell=True)
                 observedclients.append(p.addr2)
 
 # With the sniffmgmt() function complete, we can invoke the Scapy sniff()
